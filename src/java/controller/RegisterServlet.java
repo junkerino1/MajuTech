3f9956ecd75cde8ee10a35a318e8fc6fa15522ee
junkerino1 <ooijunkang2005@gmail.com>
2025-04-26 20:51:36 +0800

    fixed username unique issue

--- src/java/controller/RegisterServlet.java
+++ src/java/controller/RegisterServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.transaction.UserTransaction;
 import java.io.IOException;
+import java.util.List;
 
 import model.User;
 import service.UserService;
@@ -30,9 +31,18 @@ public class RegisterServlet extends HttpServlet {
             String gender = request.getParameter("gender");
 
             if (!password.equals(confirmPassword)) {
-                request.setAttribute("error", "Passwords do not match!");
-                request.getRequestDispatcher("/view/register.jsp").forward(request, response);
-                return;
+                request.setAttribute("errorMessage", "Passwords are not the same!");
+                request.getRequestDispatcher("/view/login-register.jsp").forward(request, response);
+            }
+
+            List<User> userList = userService.getAllUser();
+
+            for (User u : userList) {
+
+                if (username.equals(u.getUsername())) {
+                    request.setAttribute("errorMessage", "Username existed!");
+                    request.getRequestDispatcher("/view/login-register.jsp").forward(request, response);
+                }
             }
 
             User user = new User(username, password, email, phone, gender);
